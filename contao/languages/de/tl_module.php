7ae6b5a5663e7078754458d99e1b589eb169c3e8
Carsten Goetzinger <carsten.goetzinger2.ext@deka.de>
2026-07-22 08:44:04 +0200

    fix: adapt some labels and descriptions in tl_module to better align with contao standards

--- contao/languages/de/tl_module.php
+++ contao/languages/de/tl_module.php
@@ -10,15 +10,15 @@ declare(strict_types=1);
  * @license    LGPL-3.0-or-later
  */
 
-$GLOBALS['TL_LANG']['tl_module']['viewer_legend'] = 'Viewer Einstellungen';
+$GLOBALS['TL_LANG']['tl_module']['viewer_legend'] = 'Viewer-Einstellungen';
 
 $GLOBALS['TL_LANG']['tl_module']['galleryRoot'] = ['Galerie Stammverzeichnis', 'Wähle das Verzeichnis, das die Ordner für deine Galerie enthält.'];
 $GLOBALS['TL_LANG']['tl_module']['galleryCoverImageSize'] = ['Galerie Covergröße', 'Wähle die Größe der Galerie Coverbilder.'];
 $GLOBALS['TL_LANG']['tl_module']['galleryImageSize'] = ['Galerie Bildgröße', 'Wähle die Größe der Vorschaubilder in einer Galerie.'];
 $GLOBALS['TL_LANG']['tl_module']['galleryContentTpl'] = ['Inhalts-Template', 'Template für die Inhalte der Galerie.'];
 $GLOBALS['TL_LANG']['tl_module']['galleryFolderTpl'] = ['Ordner-Template', 'Template für die Ordner der Galerie.'];
-$GLOBALS['TL_LANG']['tl_module']['galleryViewer'] = ['Galerie Viewer', 'Wähle den Viewer für die Galerie.'];
+$GLOBALS['TL_LANG']['tl_module']['galleryViewer'] = ['Bildbetrachter', 'Wähle den Viewer, der für die Anzeige einzelner Bilder in der Großansicht innerhalb einer Galerie verwendet wird.'];
 
-$GLOBALS['TL_LANG']['tl_module']['gallery_viewer']['none'] = 'Kein Viewer';
+$GLOBALS['TL_LANG']['tl_module']['gallery_viewer']['none'] = 'Kein Bildbetrachter';
 $GLOBALS['TL_LANG']['tl_module']['gallery_viewer']['lightbox'] = 'Lightbox (Contao-Standard)';
 $GLOBALS['TL_LANG']['tl_module']['gallery_viewer']['photoswipe'] = 'PhotoSwipe';
--- contao/languages/en/tl_module.php
+++ contao/languages/en/tl_module.php
@@ -10,14 +10,14 @@ declare(strict_types=1);
  * @license    LGPL-3.0-or-later
  */
 
-$GLOBALS['TL_LANG']['tl_module']['viewer_legend'] = 'Viewer Settings';
+$GLOBALS['TL_LANG']['tl_module']['viewer_legend'] = 'Viewer settings';
 
 $GLOBALS['TL_LANG']['tl_module']['galleryRoot'] = ['Gallery root folder', 'Select the folder that contains the folders of your gallery.'];
 $GLOBALS['TL_LANG']['tl_module']['galleryCoverImageSize'] = ['Gallery cover size', 'Select the size of the gallery cover images.'];
 $GLOBALS['TL_LANG']['tl_module']['galleryImageSize'] = ['Gallery image size', 'Select the size of the thumbnails in a gallery.'];
 $GLOBALS['TL_LANG']['tl_module']['galleryContentTpl'] = ['Content Template', 'Template to use for the contents of a gallery'];
 $GLOBALS['TL_LANG']['tl_module']['galleryFolderTpl'] = ['Folder Template', 'Template for the folders of a gallery'];
-$GLOBALS['TL_LANG']['tl_module']['galleryViewer'] = ['Gallery Viewer', 'Select the viewer for the gallery.'];
+$GLOBALS['TL_LANG']['tl_module']['galleryViewer'] = ['Image Viewer', 'Select the viewer to be used for displaying individual images in full-screen view within a gallery.'];
 
 $GLOBALS['TL_LANG']['tl_module']['gallery_viewer']['none'] = 'No viewer';
 $GLOBALS['TL_LANG']['tl_module']['gallery_viewer']['lightbox'] = 'Lightbox (Contao Default)';
